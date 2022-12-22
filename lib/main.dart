6526ef386d9527439f46ba1ead35a496862be91e
junjiejean017 <wangjunjiejean@gmail.com>
2022-12-22 15:19:29 +0100

    Update : body set cart

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,59 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.blue,
           actions: const <Widget>[Icon(Icons.more_vert)],
         ),
+        body: Container(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Card(
+                elevation: 5,
+                color: Colors.blue,
+                child: Container(
+                  height: 150,
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: <Widget>[
+                      Image.asset(
+                        'assets/images/lyon.jpeg',
+                        fit: BoxFit.cover,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                          children: <Widget>[
+                            Expanded(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: const <Widget>[
+                                  Icon(
+                                    Icons.star_border,
+                                    size: 30,
+                                    color: Colors.white,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Row(
+                              children: const <Widget>[
+                                Text(
+                                  'Paris',
+                                  style: TextStyle(
+                                      fontSize: 30, color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
